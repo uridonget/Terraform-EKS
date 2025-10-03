d16de79bf8732a79236a1bca76dfc78897cad854
uridonget <100407513+uridonget@users.noreply.github.com>
2025-10-03 23:13:12 +0900

    Split main.tf file

--- eks.tf
+++ eks.tf
@@ -1,32 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "5.5.2"
-
-  name = "my-eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
+# eks.tf
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
---
+++ providers.tf
@@ -0,0 +1,14 @@
+# providers.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
